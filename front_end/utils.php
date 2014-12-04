fc04de989eba1082df06fde17e77d4c909f5a4d6
ejusticejr <ejusticejr@radford.edu>
2014-12-04 18:27:30 -0500

    Update utils.php

--- front_end/utils.php
+++ front_end/utils.php
@@ -51,7 +51,7 @@ function createEntry()
 	
 	$qu = 'INSERT INTO Posts (Region, Platform, Game, Event, Username, Note, PostTime) VALUES ("'.$region.'", "'.$plaform.'", "'.$game.'", "'.$event.'", "'.$username.'", "'.$note.'", "'.$postTime.'")';
 	$ins = mysqli_query($conn, $qu);
-	
+	echo "test";
 	if ($ins)
 	{
 		echo "<table class='mytableN'>";
