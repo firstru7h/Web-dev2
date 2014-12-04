da67a94f35b3ecce33d90ddecf6130256adbdcbf
ejusticejr <ejusticejr@radford.edu>
2014-12-04 18:10:01 -0500

    Update utils.php

--- front_end/utils.php
+++ front_end/utils.php
@@ -55,9 +55,9 @@ function createEntry()
 	if ($ins)
 	{
 		$qu = 'SELECT * FROM OpenLFG.Posts WHERE Region="'.$region.'", Platform="'.$platform.'", Game="'.$game.'", Event="'.$event.'", Username="'.$username.'", Note="'.$note.'", PostTime="'.$postTime.'"'
+		echo $qu;
 		$table = mysqli_query($conn, $qu);
 		echo "<table class='mytableN'>";
-		echo $qu;
 		while($row = mysql_fetch_array($table))
 		{ 
 			echo "<tr><td colspan='3' class='title'>" . $row['Event'] . "</td></tr>";
