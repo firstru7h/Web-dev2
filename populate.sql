d4c63328090034e9174d2ae39f1f62a0277e0670
Mason Mabardy <mmmabardy@radford.edu>
2014-12-10 08:12:30 -0500

    Template for populating
    Change the values, keep the now() - interval 25 minute

--- populate.sql
+++ populate.sql
@@ -1,2 +1,3 @@
-INSERT INTO OpenLFG.Posts (Region, Platform, Game, Event, Username, Note)
-Values ();
+INSERT INTO OpenLFG.Posts (PostID,Region, Platform, Game, Event, Username, Note,PostTime)
+Values (1001,"North America", "XB360", "Halo 3", "Team doubles", "Teh 7ank", "",
+ NOW() - INTERVAL 25 MINUTE);
