0051e1fc8220a8adcde5fca3b3936dbb2059914a
Mason Mabardy <mmmabardy@radford.edu>
2014-12-10 08:21:28 -0500

    Now with more samples

--- populate.sql
+++ populate.sql
@@ -1,3 +1,15 @@
 INSERT INTO OpenLFG.Posts (PostID,Region, Platform, Game, Event, Username, Note,PostTime)
-Values (1001,"North America", "XB360", "Halo 3", "Team doubles", "Teh 7ank", "",
+Values (1001,"North America", "XB360", "Halo 3", "Team doubles", "Teh 7ank", "No mic",
+ NOW() - INTERVAL 25 MINUTE);
+ INSERT INTO OpenLFG.Posts (PostID,Region, Platform, Game, Event, Username, Note,PostTime)
+Values (1002,"North America", "XB1", "Halo MCC", "Team doubles", "Teh 7ank", "Have mic",
+ NOW() - INTERVAL 25 MINUTE);
+ INSERT INTO OpenLFG.Posts (PostID,Region, Platform, Game, Event, Username, Note,PostTime)
+Values (1003,"North America", "PS4", "Destiny", "LF1M Vault of Glass", "iTza7ank", "Need mic",
+ NOW() - INTERVAL 25 MINUTE);
+ INSERT INTO OpenLFG.Posts (PostID,Region, Platform, Game, Event, Username, Note,PostTime)
+Values (1004,"North America", "PC", "World of Warcraft", "Tank LFG Highmaul", "Hlin", "Have vent and mumble",
+ NOW() - INTERVAL 25 MINUTE);
+ INSERT INTO OpenLFG.Posts (PostID,Region, Platform, Game, Event, Username, Note,PostTime)
+Values (1005,"North America", "PS3", "GTA V", "Heists", "iTza7ank", "",
  NOW() - INTERVAL 25 MINUTE);
